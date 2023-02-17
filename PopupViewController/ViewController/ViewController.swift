52d0e0b95bc4da41a726e095d7e90e4e3f11b824
Md Khaled Hasan Manna <manna@sharetrip.net>
2023-02-17 19:48:30 +0600

    present viewcontroller

--- PopupViewController/ViewController/ViewController.swift
+++ PopupViewController/ViewController/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction private func clickHere(_ sender: UIButton) {
+        showPopupView()
+    }
+    
+    private func showPopupView() {
+        let popupView = self.storyboard?.instantiateViewController(withIdentifier: "PopupViewController") as! PopupViewController
+        
+        popupView.callback.didTapClose = {[weak self] in
+            self?.dismiss(animated: true)
+        }
+        popupView.modalPresentationStyle = .currentContext
+        popupView.modalTransitionStyle = .crossDissolve
+        
+        self.present(popupView, animated: true)
+    }
 }
 
